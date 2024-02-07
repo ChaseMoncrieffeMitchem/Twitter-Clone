608e3d9e96409237981b04f88cbdd7ebf0a5deb2
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-02-06 23:47:51 -0600

    beginning twitter clone

---
+++ components/TweetInput.js
@@ -0,0 +1,14 @@
+export default function TweetInput() {
+    return (
+        <div className="flex">
+            <img 
+            className="w-11 h-11 rounded-full object-cover"
+            src="/assets/kylie.png" />
+
+            <div>
+                <textarea />
+            </div>
+
+        </div>
+    )
+}
